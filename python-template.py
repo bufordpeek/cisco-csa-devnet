9ac2dd7fca8c573edfbe83f806c9781f0f244aad
root <root@bpeek-dev-server.bpeek-labs.local>
2020-04-06 19:16:34 -0400

    More changes to the base python script

--- python-template.py
+++ python-template.py
@@ -3,6 +3,25 @@
 # General imports
 import os
 import sys
+import traceback
 import getopt
 
+def Main(args):
+	try:
+				
+		return(0)	
 
+	except Exception as X:
+		print(X)
+		raise(traceback.format_exc())
+		
+
+	finally:
+		print("Cisco Systems, Inc (c) (2020)")
+
+# Usage
+def usage():
+	print (sys.argv[0])
+
+if ( __name__ == '__main__' ):
+	sys.exit(Main(sys.argv[1:]))
